b9951faeb7a8c665fea217a14404052eef513473
Anubhav Singh <anubhavsinghab1@gmail.com>
2025-04-08 15:54:39 +0530

    Update db.js

--- Food/backend/config/db.js
+++ Food/backend/config/db.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose"
 
 export const connectDB=async()=>{
-    // await mongoose.connect('mongodb+srv://dbuser01:zSKqEWzzIaavBMKE@mycluster.85pic.mongodb.net/Foodweb?').then(()=>{
-    //     console.log("Database connected");
-// })
-
-    await mongoose.connect('mongodb://localhost:27017/Food').then(()=>{
+    await mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("Database connected to the server");
             
         })
